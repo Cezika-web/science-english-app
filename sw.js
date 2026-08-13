db659608013dbf1f2163d2c6f8ab54d20a8eb1f2
CzkEnglish <cmo.sep@gmail.com>
2026-08-13 16:36:20 -0300

    docs: explain weekly challenge schedule

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-const CACHE = 'czk-v11';
+const CACHE = 'czk-v12';
 const SHELL = [
   '/science-english-app/',
   '/science-english-app/index.html',
