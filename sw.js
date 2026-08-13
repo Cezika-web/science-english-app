893a257f01145a03227bf7cc0c43b02f01a63e68
CzkEnglish <cmo.sep@gmail.com>
2026-08-13 17:12:20 -0300

    style: compact challenge progress card

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-const CACHE = 'czk-v15';
+const CACHE = 'czk-v16';
 const SHELL = [
   '/science-english-app/',
   '/science-english-app/index.html',
