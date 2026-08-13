63494cb3ef300dc72f08c3cff61bd577e9883481
CzkEnglish <cmo.sep@gmail.com>
2026-08-13 16:11:46 -0300

    chore: refresh private challenge cache

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-const CACHE = 'czk-v9';
+const CACHE = 'czk-v10';
 const SHELL = [
   '/science-english-app/',
   '/science-english-app/index.html',
